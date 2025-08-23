d98ad959ba59a00d1453ea68339be52ab397c456
Gustavo <gu1804@hotmail.com>
2025-08-22 23:49:24 -0300

    instalattion of adminlte

---
+++ resources/views/assunto/index.blade.php
@@ -0,0 +1,42 @@
+@extends('common.root')
+
+@section('content_header')
+    <h1>Assuntos</h1>
+@stop
+
+@section('content')
+    @php
+    $heads = [
+        '#',
+        'Descrição',
+        ['label' => 'Ações', 'no-export' => true, 'width' => 5],
+    ];
+
+    $config = [
+        'order' => [[0, 'asc']],
+        'columns' => [null, null, null, ['orderable' => false]],
+    ];
+    @endphp
+
+    <x-adminlte-datatable id="table1" :heads="$heads" :config="$config" striped hoverable with-buttons>
+        @foreach($assuntos as $assunto)
+            <tr>
+                <td>{{ $assunto->codAs }}</td>
+                <td>{{ $assunto->Descricao }}</td>
+                <td>
+                    <button class="btn btn-xs btn-primary" title="Editar" onclick="window.location=''">
+                        <i class="fas fa-lg fa-edit"></i>
+                    </button>
+                </td>
+            </tr>
+        @endforeach
+    </x-adminlte-datatable>
+
+    {!! $assuntos->links('pagination::bootstrap-5') !!}
+
+    @if(session('success'))
+        <x-adminlte-alert theme="success" title="Sucesso" dismissable>
+            {{ session('success') }}
+        </x-adminlte-alert>
+    @endif
+@stop
